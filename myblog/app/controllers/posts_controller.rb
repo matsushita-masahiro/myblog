0ff7d9e8eff41a133eaf6ac531ea45f5f31f190c
masahiro <mmatsu3737@gmail.com>
2018-07-10 02:38:07 +0900

    create myblog

---
+++ myblog/app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+
+  # Convention over Configuration (CoC)
+
+  def index
+    @posts = Post.all.order(created_at: 'desc')
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    # save
+    # @post = Post.new(params[:post])
+    # @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = Post.new(post_params)
+    if @post.save
+      # redirect
+      redirect_to posts_path
+    else
+      # render plain: @post.errors.inspect
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+end
